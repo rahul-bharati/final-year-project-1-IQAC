437ed6d65ec937fd32fe4be5336000e63ccb1c41
Rahul Bharati <rahulrbharati@gmail.com>
2018-08-07 08:25:06 +0530

    completed web application

--- config/config.php
+++ config/config.php
@@ -1,10 +1,11 @@
 <?php
-    define("DB_SERVER","localhost");
-    define("DB_USER","root");
-    define("DB_PASS","72485");
+    define("DB_SERVER","103.21.58.5");
+    define("DB_USER","rahul_b");
+    define("DB_PASS","Rahul@8080");
     define("DB_NAME","iqac");
+    define("DB_PORT","3306");
 
-    $con = new mysqli(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+    $con = new mysqli(DB_SERVER,DB_USER,DB_PASS,DB_NAME,DB_PORT);
 
     if($con->connect_error)
     {
